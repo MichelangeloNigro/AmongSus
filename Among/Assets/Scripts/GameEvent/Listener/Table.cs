fa61a0dba8b4ad82c5c777a948b9b5d2879c8028
Diciamo michelangelo <mikenigro99@gmail.com>
2021-04-18 18:25:32 +0200

    Added Task slider

--- Among/Assets/Scripts/GameEvent/Listener/Table.cs
+++ Among/Assets/Scripts/GameEvent/Listener/Table.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Table : Riutilizzabile.AbstractGameEventListener
 {
     [SerializeField] GameObject ReportRable;
-
     protected override void OnEventRaised()
     {
+ 
         ReportRable.SetActive(true);
     }
+  
 }
--- Among/Assets/Scripts/Manager.cs
+++ Among/Assets/Scripts/Manager.cs
@@ -1,29 +1,39 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : Riutilizzabile.Singleton<Manager>
 {
-    public int TaskCompleted;
+    [HideInInspector] public float TaskCompleted;
+    [HideInInspector] public float TotalTask;
     public GameObject currentminigame;
     public GameObject currentBody;
     public GameObject LocalPlayer;
     public GameObject good;
     public GameObject impostor;
     public float TimeForTable;
+    public float TimeForvotatione;
+    [SerializeField] Button table;
+    public Riutilizzabile.GameEvent timerTable;
+    public Coroutine curr_corutine;
     private void Start()
     {
         if (LocalPlayer.GetComponent<Movement>().data.tipo == PlayerCustom.type.Normal)
         {
-            good.SetActive( true);
+            good.SetActive(true);
         }
         if (LocalPlayer.GetComponent<Movement>().data.tipo == PlayerCustom.type.Impostor)
         {
             impostor.SetActive(true);
         }
+        timerTable.Raise();
+        TotalTask = LocalPlayer.GetComponent<Movement>().data.TaskPerInstance;
     }
-    IEnumerator Timer()
+    public IEnumerator Timer()
     {
+        table.interactable = false;
+        yield return new WaitForSeconds(TimeForTable);
+        table.interactable = true;
 
     }
 }
