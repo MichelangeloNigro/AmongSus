6c8444cd137719940bfadbef3a2b5ff0ecc90720
mikenigro <mikenigro99@gmail.com>
2021-04-19 01:59:56 +0200

    Vents

---
+++ Among/Assets/Scripts/UI/Arrow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] GameObject player;
+    public void freccia()
+    {
+        player.transform.position = target.transform.position;
+    }
+}
--- Among/Assets/Scripts/UI/ImpostorButtons.cs
+++ Among/Assets/Scripts/UI/ImpostorButtons.cs
@@ -8,15 +8,47 @@ public class ImpostorButtons : MonoBehaviour
     [SerializeField] GameObject prefabBody;
     [SerializeField] GameObject characterSprite;
     [SerializeField] Button killB;
-public void kill()
+    [SerializeField] GameObject[] arrows;
+   
+    bool isinVent = false;
+    public void kill()
     {
         killB.interactable = false;
-    var body=    Instantiate(prefabBody,Manager.Instance.currentTarget.transform.position,Quaternion.identity);
+        var body = Instantiate(prefabBody, Manager.Instance.currentTarget.transform.position, Quaternion.identity);
         body.GetComponent<SpriteRenderer>().color = Manager.Instance.currentTarget.GetComponent<Movement>().data.colore;
         Destroy(Manager.Instance.currentTarget);
     }
     public void Vent()
     {
-        characterSprite.SetActive(false);
+        if (!isinVent)
+        {
+            InVent();
+        }
+        else
+        {
+            OutVent();
+        }
+
     }
+
+    private void InVent()
+    {
+        characterSprite.SetActive(false);
+        isinVent = true;
+        foreach (GameObject item in arrows)
+        {
+            item.SetActive(true);
+        }
+    }
+    private void OutVent()
+    {
+        characterSprite.SetActive(true);
+        isinVent = false;
+        foreach (GameObject item in arrows)
+        {
+            item.SetActive(false);
+        }
+    }
+  
+   
 }
