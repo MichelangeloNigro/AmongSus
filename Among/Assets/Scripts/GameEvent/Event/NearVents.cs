ea3a53d0dc0e788abb1c5a408709e9732052f46a
mikenigro <mikenigro99@gmail.com>
2021-04-18 22:32:17 +0200

    Camera

---
+++ Among/Assets/Scripts/GameEvent/Event/NearVents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearVents : MonoBehaviour
+{
+    [SerializeField] Riutilizzabile.GameEvent nearVents;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            nearVents.Raise();
+            Manager.Instance.currentminigame = other.GetComponent<MinigameHolder>().prefabMinigame;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            nearVents.Raise();
+            Manager.Instance.currentminigame = null;
+        }
+    }
+}
